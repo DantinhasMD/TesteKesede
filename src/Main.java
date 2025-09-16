fa69daaf5f0d67d7525994a69e203e95a11c4997
Maria <eduarda25.mecd@gmail.com>
2025-09-16 15:57:01 -0300

    Testando

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static int soma(int a, int b) {
+        return a+b;
+    }
     public static void main(String[] args) {
-        System.out.println("Teste");
+        System.out.printf("Resultado: %02d", soma(5,8));
     }
 }
